d1e52422ff3a9d2287a36e77a1a5564e2a1dd8c6
Huthifakhraishah <huthifakhraishah98@gmail.com>
2024-04-08 00:21:44 +0200

    feat: enhance src/index.ts with CORS, logging, and additional middleware support

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,20 +3,27 @@ import dotenv from "dotenv";
 import { errorHandler } from "./middleware/errorMiddleware";
 import checkJwt from "./middleware/auth";
 import taskRoutes from "./routes/taskRoutes";
+import cors from "cors";
+import morgan from "morgan";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
 
+// Middleware for parsing JSON request bodies
 app.use(express.json());
+app.use(cors());
+app.use(morgan("combined"));
 
 app.get("/public", (req, res) => {
   res.status(200).send("Public Content");
 });
 
+// Applying the Auth0 JWT middleware to secure task-related routes
 app.use("/api", checkJwt, taskRoutes);
 
+// Centralized error handling middleware
 app.use(errorHandler);
 
 app.listen(port, () => {
