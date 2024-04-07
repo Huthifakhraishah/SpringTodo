85338684718ed1ecef7274b7400b546e351a73bd
Huthifakhraishah <huthifakhraishah98@gmail.com>
2024-04-08 01:25:42 +0200

    fix: updating docker related process and run

--- init-db.sql
+++ init-db.sql
@@ -14,28 +14,10 @@ CREATE TABLE IF NOT EXISTS tasks (
     status task_status,
     deadline TIMESTAMP,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-CREATE TABLE IF NOT EXISTS users (
-    user_id SERIAL PRIMARY KEY,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    auth0_id VARCHAR(255) UNIQUE
-);
-
-CREATE TYPE task_status AS ENUM ('TODO', 'IN PROGRESS', 'COMPLETED');
-
-CREATE TABLE IF NOT EXISTS tasks (
-    task_id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users(user_id),
-    title VARCHAR(255) NOT NULL,
-    description TEXT,
-    status task_status,
-    deadline TIMESTAMP,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Create a function to update the timestamp
+-- Create a function to update the 'updated_at' timestamp
 CREATE OR REPLACE FUNCTION update_modified_column()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -44,7 +26,7 @@ BEGIN
 END;
 $$ language 'plpgsql';
 
--- Create a trigger that uses the function
+-- Create a trigger that uses the function for the 'tasks' table
 CREATE TRIGGER update_tasks_modtime
 BEFORE UPDATE ON tasks
 FOR EACH ROW
